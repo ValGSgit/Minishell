73223baf368dbcb028640f7d7b7af46c633780a0
ValGSgit <entinosusini@gmail.com>
2025-04-29 16:11:49 +0200

    update push

---
+++ src/env_utils2.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_utils2.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vagarcia <vagarcia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/03 11:58:27 by vagarcia          #+#    #+#             */
+/*   Updated: 2025/04/28 14:45:00 by vagarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+char	**copy_env(char **envp)
+{
+	int		i;
+	char	**env;
+
+	i = 0;
+	if (!envp)
+		return (NULL);
+	while (envp[i])
+		i++;
+	env = malloc(sizeof(char *) * (i + 1));
+	if (!env)
+		return (NULL);
+	i = 0;
+	while (envp[i])
+	{
+		env[i] = ft_strdup(envp[i]);
+		if (!env[i])
+		{
+			free_env(env);
+			return (NULL);
+		}
+		i++;
+	}
+	env[i] = NULL;
+	return (env);
+}
+
+char	*get_env_value(char *name, char **env)
+{
+	int	i;
+	int	len;
+
+	if (!name || !env)
+		return (NULL);
+	len = ft_strlen(name);
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strncmp(env[i], name, len) == 0 && env[i][len] == '=')
+			return (env[i] + len + 1);
+		i++;
+	}
+	return (NULL);
+}
+
+void	add_to_env(char ***env, char *new_entry)
+{
+	char	**new_env;
+	int		i;
+	int		size;
+
+	if (!new_entry)
+		return ;
+	size = 0;
+	while ((*env)[size])
+		size++;
+	new_env = malloc(sizeof(char *) * (size + 2));
+	if (!new_env)
+	{
+		free(new_entry);
+		return ;
+	}
+	i = 0;
+	while ((*env)[i])
+	{
+		new_env[i] = (*env)[i];
+		i++;
+	}
+	new_env[i] = new_entry;
+	new_env[i + 1] = NULL;
+	free(*env);
+	*env = new_env;
+}
+
---
+++ src/heredocs_more_utils.c
@@ -0,0 +1,152 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredocs_more_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vagarcia <vagarcia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/14 11:21:46 by vagarcia          #+#    #+#             */
+/*   Updated: 2025/04/14 11:27:24 by vagarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+char *process_da_bullshit(char *arg, t_shell *shell)
+{
+	t_expander_state	dabullshit;
+	int				i;
+
+	ft_memset(&dabullshit, 0, sizeof(t_expander_state));
+	dabullshit.result = ft_strdup("");
+	if (!dabullshit.result || !arg)
+		return (dabullshit.result);
+	i = 0;
+	while (arg[i])
+	{
+		if (arg[i] == '$')
+		{
+			i = handle_dollar_sign(arg, i, &dabullshit, shell);
+			i++;
+		}
+		else
+		{
+			dabullshit.result = append_str(dabullshit.result, (char[]){arg[i], '\0'});
+			(i)++;
+		}
+	}
+	return (dabullshit.result);
+}
+
+void	read_heredoc_input(const char *delimiter, int fd, t_cmd *cmd, bool expand_vars)
+{
+	char	*line;
+	char	*processed_line;
+
+	while (1)
+	{
+		line = readline("> ");
+		if (!line)
+		{
+			ft_putstr_fd("warning: here-document delimited by end-of-file (wanted `", 2);
+			ft_putstr_fd((char *)delimiter, 2);
+			ft_putstr_fd("')\n", 2);
+			break;
+		}
+		if (ft_strcmp(line, delimiter) == 0 && ft_strlen(line) == ft_strlen(delimiter))
+		{
+			free(line);
+			break;
+		}
+		if (expand_vars)
+		{
+			processed_line = process_da_bullshit(line, cmd->shell);
+			free(line);
+			line = NULL;
+		}
+		else
+			processed_line = line;
+		if (processed_line)
+		{
+			write(fd, processed_line, ft_strlen(processed_line));
+			write(fd, "\n", 1);
+			free(processed_line);
+		}
+	}
+}
+
+void	heredoc_child_process(t_cmd *cmd, char *clean_eof, int fd, bool expand_vars)
+{
+	setup_heredoc_signals();
+	read_heredoc_input(clean_eof, fd, cmd, expand_vars);
+	close(fd);
+	free(clean_eof);
+	cleanup_shell(cmd->shell);
+	exit(0);
+}
+
+int	heredoc_parent_process(pid_t pid, t_cmd *cmd, int fd, char *clean_eof)
+{
+	int	status;
+	int	exit_code;
+
+	close(fd);
+	free(clean_eof);
+	waitpid(pid, &status, 0);
+	if (WIFSIGNALED(status))
+	{
+		cmd->shell->exit_status = 128 + WTERMSIG(status);
+		if (WTERMSIG(status) == SIGINT)
+		{
+			cmd->shell->signal_status = 1;
+			cmd->shell->exit_status = 130;
+			g_signal_received = 130;
+			close(fd);
+			return (1);
+		}
+	}
+	else if (WIFEXITED(status))
+	{
+		exit_code = WEXITSTATUS(status);
+		if (exit_code == 130)
+		{
+			cmd->shell->exit_status = 130;
+			g_signal_received = 130;
+			close(fd);
+			return (1);
+		}
+		cmd->shell->exit_status = exit_code;
+	}
+	return (0);
+}
+
+char	*handle_expanded_line(char *arg, char *expanded_line)
+{
+	if (ft_strcmp(expanded_line, arg) != 0)
+	{
+		free(arg);
+		return (expanded_line);
+	}
+	return (arg);
+}
+
+bool	check_delimiter_quotes(char *eof)
+{
+	int		i;
+	bool	has_quotes;
+
+	if (!eof || !*eof)
+		return (true);
+	if ((eof[0] == '\'' && eof[1] == '\'' && !eof[2]) || 
+		(eof[0] == '"' && eof[1] == '"' && !eof[2]))
+		return (false);
+	i = 0;
+	has_quotes = false;
+	while (eof[i])
+	{
+		if (eof[i] == '\'' || eof[i] == '"')
+			has_quotes = true;
+		i++;
+	}
+	return (!has_quotes);
+}
