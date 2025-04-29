8546654192dcb7dc6360dc98b2d6c5e5250edad4
Irina Maria Ionescu <iionescu@c3r9p5.42vienna.com>
2025-04-29 18:03:45 +0200

    lastest

---
+++ latest/src/builtins_utils.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vagarcia <vagarcia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/02 10:33:07 by vagarcia          #+#    #+#             */
+/*   Updated: 2025/04/28 14:10:00 by vagarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+static void	update_oldpwd(t_shell *shell, char *old_pwd)
+{
+	char	*oldpwd_var;
+
+	if (old_pwd)
+	{
+		oldpwd_var = ft_strjoin("OLDPWD=", old_pwd);
+		if (oldpwd_var)
+		{
+			update_or_add_env(oldpwd_var, &shell->env);
+			free(oldpwd_var);
+		}
+	}
+}
+
+void	update_pwd(t_shell *shell)
+{
+	char	*cwd;
+	char	*new_pwd;
+	char	*old_pwd;
+
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
+	{
+		old_pwd = get_env_value("PWD", shell->env);
+		update_oldpwd(shell, old_pwd);
+		return ;
+	}
+	old_pwd = get_env_value("PWD", shell->env);
+	update_oldpwd(shell, old_pwd);
+	new_pwd = ft_strjoin("PWD=", cwd);
+	if (new_pwd)
+	{
+		update_or_add_env(new_pwd, &shell->env);
+		free(new_pwd);
+	}
+	free(cwd);
+}
+
+void	update_shlvl(t_shell *shell)
+{
+	int		level;
+	char	*level_str;
+	char	*new_shlvl;
+	char	*val;
+
+	if (!shell->env)
+		return ;
+	val = get_env_value("SHLVL", shell->env);
+	if (!val)
+	{
+		new_shlvl = ft_strdup("SHLVL=1");
+		if (new_shlvl)
+			update_or_add_env(new_shlvl, &shell->env);
+		free(new_shlvl);
+		return ;
+	}
+	level = ft_atoi(val);
+	if (level < 0)
+		level = 0;
+	level++;
+	level_str = ft_itoa(level);
+	if (!level_str)
+		return ;
+	new_shlvl = ft_strjoin("SHLVL=", level_str);
+	free(level_str);
+	if (new_shlvl)
+	{
+		update_or_add_env(new_shlvl, &shell->env);
+		free(new_shlvl);
+	}
+}
+
+void	ft_pwd(t_cmd *cmd)
+{
+	char	cwd[1024];
+
+	if (getcwd(cwd, 1024) != NULL)
+	{
+		ft_putendl_fd(cwd, STDOUT_FILENO);
+		cmd->shell->exit_status = 0;
+	}
+	else
+	{
+		ft_putstr_fd("minishell: pwd: ", 2);
+		ft_putstr_fd(strerror(errno), 2);
+		ft_putstr_fd("\n", 2);
+		cmd->shell->exit_status = 1;
+	}
+}
