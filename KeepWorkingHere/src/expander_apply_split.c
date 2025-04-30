ac8f5d2e5463e4485cbae98ee938e9444960c3a5
Valentino <entinosusini@gmail.com>
2025-04-30 14:02:24 +0200

    Add files via upload

---
+++ KeepWorkingHere/src/expander_apply_split.c
@@ -0,0 +1,159 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expander_apply_split.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vagarcia <vagarcia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/14 11:35:22 by vagarcia          #+#    #+#             */
+/*   Updated: 2025/04/14 12:44:16 by vagarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+/**
+ * Checks if a string needs word splitting (contains $ outside quotes)
+ */
+static bool	needs_word_splitting(char *str)
+{
+	int		i;
+	bool	in_quote;
+	bool	in_dquote;
+
+	in_quote = false;
+	in_dquote = false;
+	i = 0;
+	while (str && str[i])
+	{
+		if (str[i] == '\'' && !in_dquote)
+			in_quote = !in_quote;
+		else if (str[i] == '"' && !in_quote)
+			in_dquote = !in_dquote;
+		else if (str[i] == '$' && !in_quote)
+			return (true);
+		i++;
+	}
+	return (false);
+}
+
+/**
+ * Counts how many args will be in the result after word splitting
+ */
+static int	count_args_after_splitting(char **args, t_shell *shell)
+{
+	int		i;
+	int		j;
+	int		count;
+	char	**word_tokens;
+	char	*expanded;
+
+	count = 0;
+	i = 0;
+	while (args && args[i])
+	{
+		expanded = process_argument(args[i], shell);
+		if (!expanded || !needs_word_splitting(args[i]))
+			count++;
+		else if ((word_tokens = split_expanded_variable(expanded)))
+		{
+			j = 0;
+			while (word_tokens[j])
+			{
+				count++;
+				j++;
+			}
+			// Free both the array and its contents since they're not being kept
+			free_tokens(word_tokens);
+		}
+		else
+			count++;
+		if (expanded)
+			free(expanded);
+		i++;
+	}
+	return (count);
+}
+
+/**
+ * Process one argument for word splitting
+ */
+static void	process_arg_for_splitting(char *arg, char *expanded, int *count,
+		char **new_args)
+{
+	char	**word_tokens;
+	int		j;
+
+	if (!expanded)
+		return ;
+	if (!needs_word_splitting(arg))
+	{
+		new_args[(*count)++] = expanded;
+		return ;
+	}
+	word_tokens = split_expanded_variable(expanded);
+	if (!word_tokens)
+	{
+		new_args[(*count)++] = expanded;
+		return ;
+	}
+	j = 0;
+	while (word_tokens[j])
+	{
+		new_args[(*count)++] = ft_strdup(word_tokens[j]);
+		j++;
+	}
+	// Free both the array and its contents
+	free_tokens(word_tokens);
+	free(expanded);
+}
+
+/**
+ * Build the new arguments array after word splitting
+ */
+static char	**build_args_after_splitting(char **args, int max_count,
+		t_shell *shell)
+{
+	int		i;
+	int		count;
+	char	**new_args;
+	char	*expanded;
+
+	new_args = malloc((max_count + 1) * sizeof(char *));
+	if (!new_args)
+		return (args);
+	count = 0;
+	i = 0;
+	while (args && args[i])
+	{
+		expanded = process_argument(args[i], shell);
+		process_arg_for_splitting(args[i], expanded, &count, new_args);
+		i++;
+	}
+	// Ensure the array is properly NULL-terminated
+	new_args[count] = NULL;
+	int k = 0;
+	while (args[k])
+	{
+		free(args[k]);
+		k++;
+	}
+	free(args);
+	return (new_args);
+}
+
+/**
+ * Applies word splitting to expanded variables when not in quotes
+ * Returns a new expanded arguments array
+ */
+char	**apply_word_splitting(char **args, t_shell *shell)
+{
+	int		count;
+	char	**new_args;
+
+	if (!args)
+		return (NULL);
+	count = count_args_after_splitting(args, shell);
+	new_args = build_args_after_splitting(args, count, shell);
+	return (new_args);
+}
---
+++ KeepWorkingHere/src/sighandle.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sighandle.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vagarcia <vagarcia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/31 13:19:53 by vagarcia          #+#    #+#             */
+/*   Updated: 2025/04/30 10:15:00 by vagarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+volatile sig_atomic_t	g_signal_received = 0;
+
+void	setup_signals(void)
+{
+	signal(SIGINT, handle_sigint);
+	signal(SIGQUIT, handle_sigquit);
+}
+
+void	handle_sigint(int sig)
+{
+	(void)sig;
+	g_signal_received = 130;
+	write(1, "\n", 1);
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
+}
+
+void	handle_sigquit(int sig)
+{
+	(void)sig;
+	write(1, "\b\b  \b\b", 6);
+	rl_on_new_line();
+	rl_redisplay();
+}
+
+void	reset_signals(void)
+{
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+}
+
+void	ignore_signals(void)
+{
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
+	signal(SIGTERM, SIG_IGN);
+}
